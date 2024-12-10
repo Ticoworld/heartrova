faed82d8e51dc34bd421c152b3c151b2565daba8
Ticoworld <timothyticoworld@gmail.com>
2024-12-10 16:56:45 +0100

    desire img

--- src/components/Desires.jsx
+++ src/components/Desires.jsx
@@ -6,14 +6,14 @@ const Desires = () => {
     <div className="lg:px-32 lg:py-24 py-12 px-10 bg-white min-h-[90vh]">
       <div className="flex flex-col lg:flex-row justify-between items-center  gap-20">
         <div className="flex justify-center gap-4 lg:gap-2">
-          <div className="md:w-[250px] sm:w-[200px] lg:w-[150px] w-[200px] overflow-hidden rounded-lg shadow-md">
+          <div className="md:w-[250px] sm:w-[200px] lg:w-[200px] w-[200px] overflow-hidden rounded-lg shadow-md">
             <img
               src="/man.jpg"
               alt="Man"
               className="object-cover w-full h-full"
             />
           </div>
-          <div className="md:w-[250px] sm:w-[200px] lg:w-[150px] w-[200px] overflow-hidden rounded-lg shadow-md">
+          <div className="md:w-[250px] sm:w-[200px] lg:w-[200px] w-[200px] overflow-hidden rounded-lg shadow-md">
             <img
               src="/woman.jpg"
               alt="Woman"
